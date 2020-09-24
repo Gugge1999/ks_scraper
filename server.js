589efa4ca0d4d9c4a1a836e09eff25af8c563273
Gugge1999 <davidgust99@gmail.com>
2020-09-24 17:03:55 +0200

    Removed arrays and other unnecessary code

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-// ideer:
-// 1:08:06
+'use strict';
 var express = require('express');
 var fs = require('fs');
 var request = require('request');
@@ -10,43 +9,36 @@ require('dotenv').config();
 var app = express();
 
 var numberOfTimesReloded = 0;
-var indexPos = 0;
-var title, date;
-var watchArray = [];
-var dateArray = [];
 var dateAndTime = new Date().toLocaleString();
 var json = {
-  title: watchArray,
-  date: dateArray,
+  title: '',
+  date: '',
 };
 
 app.get('/scrape', function (req, res) {
   //url = 'https://klocksnack.se/forums/handla-s%C3%A4ljes-bytes.11/';
-  url = 'https://klocksnack.se/search/13278215/?q=556&t=post&o=date&c[title_only]=1&c[node]=11+50';
+  const url = 'https://klocksnack.se/search/13278215/?q=556&t=post&o=date&c[title_only]=1&c[node]=11+50';
   //url = 'https://klocksnack.se/search/13278222/?q=6139&t=post&o=date&c[title_only]=1&c[node]=11+50';
 
   request(url, function (error, response, html) {
     if (!error) {
       var $ = cheerio.load(html);
 
+      // ideer:
       var watch = $('.title')
         .children()
         .first()
         .text()
         .replace(/Tillbakadragen|Avslutad|Säljes|Bytes|\//gi, '') // Remove sale status of the watch
         .trim();
-      watchArray.push(watch);
+      json.title = watch;
 
       var date = $('.DateTime').first().text();
-      dateArray.push(date);
+      json.date = date;
     } else {
       console.log(error);
     }
-
-    var emailText = `${[watchArray[indexPos].concat(`. Upplagd: ${dateArray[indexPos]}`)]}. Skickat: ${dateAndTime}`;
-
-    json.title = watchArray[indexPos];
-    json.date = dateArray[indexPos];
+    var emailText = `${json.title}. Upplagd: ${json.date}. Skickat: ${dateAndTime}`;
 
     console.log(`json scraped data: ${JSON.stringify(json, null, 4)}`);
     var formatedJSON = JSON.stringify(json, null, 4);
@@ -74,9 +66,9 @@ app.get('/scrape', function (req, res) {
           } else {
             console.log('\u001B[34mEmail sent.');
 
-            // Parameter 1:  output.json - this is what the created filename will be called
-            // Parameter 2:  JSON.stringify(json, null, 4) - the data to write. stringify makes it more readable. 4 means it inserts 4 white spaces before the key value pair
-            // Parameter 3:  callback function - a callback function to let us know the status of our function
+            // Parameter 1: output.json - this is what the created filename will be called
+            // Parameter 2: JSON.stringify(json, null, 4) - the data to write. stringify makes it more readable. 4 means it inserts 4 white spaces before the key.
+            // Parameter 3: callback function - a callback function to let us know the status of our function
             fs.writeFile('output.json', JSON.stringify(json, null, 4), function (err) {
               console.log('File successfully written! - Check your project directory for the output.json file');
             });
@@ -86,7 +78,7 @@ app.get('/scrape', function (req, res) {
     });
 
     // Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-    res.send('Check your console!');
+    res.send('Check your console');
   });
 });
 
@@ -107,7 +99,7 @@ function msToTime(reloadTime) {
 }
 
 // This interval timer reloads localhost:8081/scrape
-var reloadTime = 10000; // 3600000 ms = 1 hour. 1800000 ms = 30 min
+var reloadTime = 5000; // 3600000 ms = 1 hour. 1800000 ms = 30 min
 setInterval(
   () =>
     request('http://localhost:8081/scrape', (err, res, body) => {
@@ -115,7 +107,7 @@ setInterval(
         return console.log(err);
       } else {
         numberOfTimesReloded++;
-        console.log(`Number of reloads: ${numberOfTimesReloded}. Site reloads every: ${msToTime(reloadTime)} (hh/mm/ss)`);
+        console.log(`Number of reloads: ${numberOfTimesReloded + 1}. Site reloads every: ${msToTime(reloadTime)} (hh/mm/ss)`);
       }
     }),
   reloadTime
